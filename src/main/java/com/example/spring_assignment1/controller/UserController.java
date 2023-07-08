c39d4a7456944cd64c707172099cceeec2bb637d
Bach <107246697+QuangBach111@users.noreply.github.com>
2023-07-08 19:41:23 +0700

    update UserService(CRUD), model User and Position

--- src/main/java/com/example/spring_assignment1/controller/UserController.java
+++ src/main/java/com/example/spring_assignment1/controller/UserController.java
@@ -3,11 +3,19 @@ package com.example.spring_assignment1.controller;
 import com.example.spring_assignment1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @GetMapping("")
+    public String listUser(Model model) {
+
+        return
+    }
 }
